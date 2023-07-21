dcc141d4aee5924545c64537963761ace7d0f4d9
Raphaella-leite <77009283+Raphaella-leite@users.noreply.github.com>
2023-07-21 09:13:39 -0300

    Add files via upload
    Simulador de idade correta para emitir CNH

---
+++ CNH- Operadores.py
@@ -0,0 +1,17 @@
+
+
+print("-----CNH-----")
+
+MAIOR_IDADE = 18
+
+idade = int(input("Digite sua idade:"))
+ 
+if idade < MAIOR_IDADE :
+    print("Você ainda não Pode tirar a CNH.")
+
+elif idade == MAIOR_IDADE:
+    print("Pode fazer aulas teóricas!")
+
+else:
+    print("Parabéns você já pode tirar a CNH.")
+
