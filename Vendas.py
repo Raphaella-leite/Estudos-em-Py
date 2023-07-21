2a3fd17ff4921f0bb15f5ecdfa3960c0cf8df406
Raphaella-leite <77009283+Raphaella-leite@users.noreply.github.com>
2023-07-21 09:11:53 -0300

    Add files via upload
    Programa que calcula as metas de vendas do mês.

---
+++ Vendas.py
@@ -0,0 +1,16 @@
+
+print('\t ----VENDAS---- ')
+
+vendas= 5000
+
+if vendas > 1000:
+ print("Parabéns, bateu a 1ª meta")
+
+elif  vendas > 3000:
+ print("Parabéns, bateu a 2ª meta, bônus de R$200 para o vendedor")
+
+elif  vendas > 4500:
+ print("Parabéns, bateu a 3ª meta, bônus de R$500 para o vendedor")
+
+elif vendas <= 1000:
+ print("Que pena, nenhuma meta batida!")
